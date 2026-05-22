4e4341331f1312407e516024811063a26385b75e
Namita Lakpoti <namitalakpoti@gmail.com>
2026-05-22 23:47:31 +0530

    Update firmware.ino

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,6 +2,7 @@
 #include <SPIFFS.h>
 #include <WiFi.h>
 #include <WebServer.h>
+
 #define MPU_ADDR 0x68
 
 WebServer server(80);
@@ -11,14 +12,16 @@ volatile float pitch = 0.0;
 volatile float roll = 0.0;
 float baseline = 0.0;
 volatile int score = 100;
-unsigned long lastLog = 0;
-
 SemaphoreHandle_t dataMutex;
 
 void handleRoot()
 {
   if (xSemaphoreTake(dataMutex, 10) == pdTRUE)
   {
+    float localPitch = pitch;
+    float localBaseline = baseline;
+    int localScore = score;
+    xSemaphoreGive(dataMutex);
 
     String html = "<!DOCTYPE html><html><head>";
     html += "<meta http-equiv='refresh' content='1'>";
@@ -27,9 +30,10 @@ void handleRoot()
     html += ".good{color:#50fa7b;font-size:40px;}.bad{color:#ff5555;font-size:40px;}";
     html += ".score{font-size:60px;font-weight:bold;}</style></head><body>";
     html += "<h1>Posture Monitor</h1>";
-    html += "<p>Pitch: " + String(pitch, 1) + "&deg;</p>";
-    html += "<p>Deviation: " + String(pitch - baseline, 1) + "&deg;</p>";
-    if (abs(pitch - baseline) > 20.0)
+    html += "<p>Pitch: " + String(localPitch, 1) + "&deg;</p>";
+    html += "<p>Deviation: " + String(localPitch - localBaseline, 1) + "&deg;</p>";
+    
+    if (abs(localPitch - localBaseline) > 20.0)
     {
       html += "<p class='bad'>BAD POSTURE</p>";
     }
@@ -37,9 +41,10 @@ void handleRoot()
     {
       html += "<p class='good'>GOOD POSTURE</p>";
     }
-    html += "<p class='score'>Score: " + String(score) + "</p>";
+    
+    html += "<p class='score'>Score: " + String(localScore) + "</p>";
     html += "</body></html>";
-    xSemaphoreGive(dataMutex);
+    
     server.send(200, "text/html", html);
   }
   else
@@ -63,6 +68,7 @@ void calibrate()
     float axf = ax / 16384.0;
     float ayf = ay / 16384.0;
     float azf = az / 16384.0;
+
     Wire.beginTransmission(MPU_ADDR);
     Wire.write(0x43);
     Wire.endTransmission(false);
@@ -84,6 +90,7 @@ void calibrate()
   Serial.print("Baseline set: ");
   Serial.println(baseline);
 }
+
 void sensorTask(void *pvParameters)
 {
   float dt = 0.01;
@@ -126,26 +133,26 @@ void sensorTask(void *pvParameters)
       if (deviation > 20.0)
       {
         score -= 2;
-        if (score < 0)
-          score = 0;
+        if (score < 0) score = 0;
       }
       else
       {
         score += 1;
-        if (score > 100)
-          score = 100;
+        if (score > 100) score = 100;
       }
       localPitch = pitch;
       localRoll = roll;
       localScore = score;
       xSemaphoreGive(dataMutex);
     }
+
     Serial.print("Pitch: ");
     Serial.print(localPitch);
     Serial.print(" Roll: ");
     Serial.print(localRoll);
     Serial.print(" Score: ");
     Serial.println(localScore);
+
     vTaskDelay(10 / portTICK_PERIOD_MS);
   }
 }
@@ -158,6 +165,7 @@ void serverTask(void *pvParameters)
     vTaskDelay(5 / portTICK_PERIOD_MS);
   }
 }
+
 void loggerTask(void *pvParameters)
 {
   for (;;)
@@ -172,7 +180,6 @@ void loggerTask(void *pvParameters)
       xSemaphoreGive(dataMutex);
     }
     File f = SPIFFS.open("/posture_log.csv", FILE_APPEND);
-
     if (f)
     {
       f.print(millis());
@@ -197,6 +204,7 @@ void setup()
   Wire.write(0x00);
   Wire.endTransmission(true);
   Serial.println("MPU6050 ready!");
+  
   if (!SPIFFS.begin(true))
   {
     Serial.println("SPIFFS failed! Continuing without logging...");
@@ -205,7 +213,9 @@ void setup()
   {
     Serial.println("SPIFFS ready!");
   }
+  
   calibrate();
+  
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -215,8 +225,10 @@ void setup()
   Serial.println("");
   Serial.print("Connect to: http://");
   Serial.println(WiFi.localIP());
+  
   server.on("/", handleRoot);
   server.begin();
+  
   dataMutex = xSemaphoreCreateMutex();
   xTaskCreate(sensorTask, "Sensor", 4096, NULL, 3, NULL);
   xTaskCreate(serverTask, "Server", 4096, NULL, 2, NULL);
